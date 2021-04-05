53860b04708e30d13582a3d5e6873d485a502bef
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-05 16:51:36 +0530

    starting base templates

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'tweets',
+    'hm',
     'rest_framework',
 ]
 
--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tweets/', include('tweets.urls')),
+    path('hm/', include('hm.urls')),
 ]
