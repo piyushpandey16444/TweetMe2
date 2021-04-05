8b7d7b7ca9855a81d0e5f62fce9bfb713193790d
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-05 16:16:28 +0530

    implimenting like btn

--- tweets/models.py
+++ tweets/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class Tweet(models.Model):
     content = models.TextField(null=True, blank=True)
+    count = models.IntegerField(null=True, blank=True, default=0)
     # image = models.ImageField(upload_to='images/', null=True, blank=True)
